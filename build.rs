cd9f0b272a60dae7ee8e34ea24efaf876c2d873a
merlinfuchs <merlinfuchs2001@gmail.com>
2022-03-25 22:55:48 +0100

    remove unnecessary build script

--- build.rs
+++
@@ -1,6 +0,0 @@
-const FRONTEND_DIR: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/frontend/build");
-
-fn main() {
-    // this forces the program to recompile when the frontend has changed
-    println!("cargo:rerun-if-changed={}/src", FRONTEND_DIR)
-}
